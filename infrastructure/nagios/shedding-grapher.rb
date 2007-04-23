08f63c6342b36af6076c3da5053a9c40b5b17347
Timothy J. Fontaine <tjfontaine@oftc.net>
2007-04-23 16:40:26 +0000

    colorization, and min length for edges to make graphs prettier

--- infrastructure/nagios/shedding-grapher.rb
+++ infrastructure/nagios/shedding-grapher.rb
@@ -8,9 +8,12 @@ DRb.start_service
 
 irc = DRbObject.new_with_uri(SERVER_URI)
 
+colors = ['red', 'blue', 'green', 'yellow', 'brown']
+
 servers = {}
 conns = {}
 versions = {}
+uqver = {}
 count = 0
 
 ret, links = irc.get_links
@@ -31,6 +34,7 @@ servers.each_key do |servername|
       value = value.split('+')[1]
       value = value[4, value.index('(')-4]
       versions[servername] = value if ret
+      uqver[value] = uqver.length unless uqver[value]
     else
       $stderr.puts "couldn't get version for #{servername} #{ret} #{value}"
     end
@@ -39,13 +43,18 @@ end
 
 puts 'graph OFTC {'
 servers.each_key do |name|
-  puts "\t#{servers[name]} [label=\"#{name.split('.')[0]}(#{versions[name]})\"];"
+  sid = servers[name]
+  sname = name.split('.')[0]
+  version = versions[name]
+  color = 'white'
+  color = colors[uqver[version]] if uqver[version]
+  puts "\t#{sid} [label=\"#{sname}(#{version})\" style=filled fillcolor=#{color}];"
 end
 
 subgraph = {}
 
 conns.each_key do |name|
-  conns[name].each { |c| puts "\t#{servers[name]} -- #{servers[c]};" unless name == c }
+  conns[name].each { |c| puts "\t#{servers[name]} -- #{servers[c]} [len=5];" unless name == c }
 end
 
 puts "}"
