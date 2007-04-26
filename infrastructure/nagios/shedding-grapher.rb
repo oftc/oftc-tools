77e52cfb799d44b4d39e008c5e7f1a324cf7564c
Timothy J. Fontaine <tjfontaine@oftc.net>
2007-04-26 23:52:29 +0000

    parse servers.yaml for hub status, mark hubs as root=true side effect circo isn't crap anymore

--- infrastructure/nagios/shedding-grapher.rb
+++ infrastructure/nagios/shedding-grapher.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/ruby
 
 require 'drb/drb'
+require 'yaml'
 
 SERVER_URI="druby://localhost:8787"
+SERVERSYAML='servers.yaml'
 
 DRb.start_service
 
@@ -10,11 +12,18 @@ irc = DRbObject.new_with_uri(SERVER_URI)
 
 colors = ['red', 'blue', 'green', 'yellow', 'brown']
 
+tmp = YAML::load(File.open(SERVERSYAML))['servers'] 
+syaml = {}
+tmp.each do |server|
+  syaml[server['name']] = server.dup
+end
+
 servers = {}
 conns = {}
 versions = {}
 uqver = {}
 users = {}
+global = 1
 
 ret, links = irc.get_links
 if ret
@@ -44,6 +53,8 @@ servers.each_key do |servername|
     if ret
       local = value[0][0].chomp
       local = local.split(':')[1].strip.to_i
+      global = value[1][0].chomp
+      global = global.split(':')[1].strip.to_i
       users[servername] = local
     else
       $stderr.puts "couldn't get users for #{servername} #{ret} #{value}"
@@ -51,19 +62,20 @@ servers.each_key do |servername|
   end
 end
 
-puts 'graph OFTC {'
-puts "\tsubgraph lgd {"
-count = servers.length + 1
-uqver.each_key do |v|
-  version = uqver[v]
-  color = 'white'
-  color = colors[version] if uqver[v]
-  puts "\t\t#{count} [label=\"#{v}\" style=filled fillcolor=#{color}];"
-  count += 1
-end
-puts "\t}"
+graph = File.open('oftc.dot', 'w')
+graph.puts 'graph OFTC {'
+#graph.puts "\tsubgraph lgd {"
+#count = servers.length + 1
+#uqver.each_key do |v|
+#  version = uqver[v]
+#  color = 'white'
+#  color = colors[version] if uqver[v]
+#  graph.puts "\t\t#{count} [label=\"#{v}\" style=filled fillcolor=#{color}];"
+#  count += 1
+#end
+#graph.puts "\t}"
 
-puts "\trankdir=BT;"
+#graph.puts "\trankdir=BT;"
 
 servers.each_key do |name|
   sid = servers[name]
@@ -72,11 +84,21 @@ servers.each_key do |name|
   color = 'white'
   color = colors[uqver[version]] if uqver[version]
   user = users[name]
-  puts "\t#{sid} [label=\"#{sname}(#{user})\" style=filled fillcolor=#{color}];"
+  pct = (user.to_f / global.to_f) * 100
+  hub = syaml[sname]['hub'] if sname != 'services'
+  region = syaml[sname]['region'] if sname != 'services'
+  graph.write "\t#{sid} [label=\"#{sname}"
+  graph.write "\\nUsers: #{user} (%0.2f%%)" % pct if user and user > 0
+  graph.write "\\n#{region}" if region
+  graph.write "\""
+  graph.write " shape=octagon" if hub == true
+  graph.write " root=true" if hub == true
+  graph.write " style=filled fillcolor=#{color}];\n"
 end
 
 conns.each_key do |name|
-  conns[name].each { |c| puts "\t#{servers[name]} -- #{servers[c]} [len=5];" unless name == c }
+  conns[name].each { |c| graph.puts "\t#{servers[name]} -- #{servers[c]};" unless name == c }
 end
 
-puts "}"
+graph.puts "}"
+graph.close
